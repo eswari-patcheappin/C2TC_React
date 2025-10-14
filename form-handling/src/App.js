556a446a4a955204e2a8a158f7d6b82b58c34223
trendingeswari <trendingeswari@gmail.com>
2025-10-14 20:16:34 +0530

    Form Handling

---
+++ form-handling/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import FormDemo from "./FormDemo"; // Import your FormDemo component
+function App() {
+  return (
+    <div>
+      <FormDemo /> {/* Render the FormDemo component */}
+    </div>
+  );
+}
+export default App;
